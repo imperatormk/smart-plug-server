1f76aae8d4e823f540047c74f21b8d1371908541
Darko <darko.simonovski@hotmail.com>
2018-05-21 22:43:06 +0200

    Fixed controllers not being mapped

--- src/main/java/com/fiktac/SmartPlugApp/SmartPlugAppApplication.java
+++ src/main/java/com/fiktac/SmartPlugApp/SmartPlugAppApplication.java
@@ -1,12 +1,9 @@
 package com.fiktac.SmartPlugApp;
 
-import com.fiktac.SmartPlugApp.device.DeviceController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = DeviceController.class)
 public class SmartPlugAppApplication {
 
 	public static void main(String[] args) {
